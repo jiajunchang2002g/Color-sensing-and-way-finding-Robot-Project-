e0574873898f32cbffc99f69814d516c9bddd0de
Terence Chan Zun Mun <zunmun@gmail.com>
2023-10-23 22:32:18 +0800

    feat: base structure

---
+++ Code/CG1111A_MBOT/sound.ino
@@ -0,0 +1,12 @@
+MeBuzzer buzzer; // create the buzzer object
+void celebrate() {
+  // Each of the following "function calls" plays a single tone.
+  // The numbers in the bracket specify the frequency and the duration (ms)
+  buzzer.tone(392, 200);
+  buzzer.tone(523, 200);
+  buzzer.tone(659, 200);
+  buzzer.tone(784, 200);
+  buzzer.tone(659, 150);
+  buzzer.tone(784, 400);
+  buzzer.noTone();
+}
---
+++ Code/CG1111A_MBOT/ultrasonic.ino
@@ -0,0 +1,27 @@
+#define TIMEOUT 2000 // Max microseconds to wait; choose according to max distance of wall
+#define SPEED_OF_SOUND 340 // Update according to your own experiment
+#define ULTRASONIC 12
+// If you are using Port 1 of mCore, the ultrasonic sensor uses digital pin 12
+// If you are using Port 2 of mCore, the ultrasonic sensor uses digital pin 10
+void u_setup() {
+ Serial.begin(9600); // to initialize the serial monitor
+}
+void u_loop() {
+  Serial.print("distance(cm) = ");
+  Serial.println(ultrasonic());
+  delay(500);
+}
+
+double ultrasonic(){
+  pinMode(ULTRASONIC, OUTPUT);
+  digitalWrite(ULTRASONIC, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC, LOW);
+
+  pinMode(ULTRASONIC, INPUT);
+  long duration = pulseIn(ULTRASONIC, HIGH, TIMEOUT);
+  double distance = duration / 2.0 / 1000000 * SPEED_OF_SOUND * 100;
+  return distance;
+}
