2a89df50da387036e82503f62d903551efd6b4ad
Wen Yee <141599443+chwenyee@users.noreply.github.com>
2023-10-29 19:17:17 +0800

    Update ir.ino

--- Code/CG1111A_MBOT/ir.ino
+++ Code/CG1111A_MBOT/ir.ino
@@ -4,21 +4,25 @@ int IREmitter = ;
 void setup() {
   pinMode(IREmitter, OUTPUT);
   pinMode(IRDetector, INPUT);
+//Serial.begin(9600);
 }
 
 void shineIR() {
   digitalWrite(IREmitter, HIGH);
 }
 
+//might need to calibrate
+
 double read_ir_distance() {
   digitalWrite(IREmitter, LOW); // get a reference reading for the ambient IR level
   delay(1000); // might need to adjust
   double reference_reading = analogRead(IRDetector);
+  
   digitalWrite(IREmitter, HIGH); // turn on emitter to read the IR detector again to obtain the signal reading including both the emitted and ambient IR.
   delay(1000); 
   double signal_reading = analogRead(IRDetector);
-  double intensity_diff = reference_reading - signal_reading;
-  double distance = 
+  double intensity_diff = reference_reading - signal_reading; //not sure which is higher
+  double distance = map(intensity_diff, 0, 1023, 0, 100);  //map(value, fromLow, fromHigh, toLow, toHigh)
 
   return distance;
 }
