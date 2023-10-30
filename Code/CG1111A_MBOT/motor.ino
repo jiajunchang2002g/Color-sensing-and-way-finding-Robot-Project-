a46f7f69458a763d60f772ea6de477d52f1a6cf9
Ethan <75615184+ecxm02@users.noreply.github.com>
2023-10-30 10:30:17 +0800

    Update motor.ino

--- Code/CG1111A_MBOT/motor.ino
+++ Code/CG1111A_MBOT/motor.ino
@@ -1,83 +1,101 @@
+#include "MeMCore.h"
 #define TURNING_TIME_MS 330 // The time duration (ms) for turning
-uint8_t motorSpeed = 255;
+#define ULTRASONIC 12
+#define TIMEOUT 1000000
+#define SPEED_OF_SOUND 330
+
+MeDCMotor leftMotor(M1); // assigning leftMotor to port M1
+MeDCMotor rightMotor(M2); // assigning RightMotor to port M2
+int left_motorSpeed = -255;
+int right_motorSpeed = 240;
 // Setting motor speed to an integer between 1 and 255
 // The larger the number, the faster the speed
 
+double ultrasonic(){ // returns distance in cm
+  pinMode(ULTRASONIC, OUTPUT);
+  digitalWrite(ULTRASONIC, LOW);
+  delayMicroseconds(2);
+  digitalWrite(ULTRASONIC, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC, LOW);
 
-void stopMotor() {
+  pinMode(ULTRASONIC, INPUT);
+  long duration = pulseIn(ULTRASONIC, HIGH, TIMEOUT);
+  double distance = duration / 2.0 / 1000000 * SPEED_OF_SOUND * 100;
+  Serial.println(distance);
+  return distance;
+}
+
+void StopMotor() {
   leftMotor.stop();
   rightMotor.stop();
 }
-
 
 void moveForward() {
-  leftMotor.run(motorSpeed);
-  rightMotor.run(motorSpeed);
+  leftMotor.run(left_motorSpeed);
+  rightMotor.run(right_motorSpeed);
 }
-
 
 void turnLeft() {
-  rightMotor.run(motorSpeed);
-  leftMotor.run(-motorSpeed);
+  rightMotor.run(right_motorSpeed);
+  leftMotor.run(-left_motorSpeed);
   delay(TURNING_TIME_MS);
-  leftMotor.stop();
-  rightMotor.stop();
+  StopMotor();
   }
 
-
 void turnRight() {
-  leftMotor.run(motorSpeed);
-  rightMotor.run(-motorSpeed);
-  delay(TURNING_TIME_MS);
-  rightMotor.stop();
-  leftMotor.stop();
+  leftMotor.run(left_motorSpeed);
+  rightMotor.run(-right_motorSpeed);
+  delay(TURNING_TIME_MS * 0.9);
+  StopMotor();
 }
-
 
 void uTurn() {
-  rightMotor.run(motorSpeed);
-  leftMotor.run(-motorSpeed);
-  delay(2 * TURNING_TIME_MS);
-  leftMotor.stop();
-  rightMotor.stop();
+  turnLeft();
+  turnLeft();
+  StopMotor();
 }
-
 
 void doubleLeftTurn() {
-  rightMotor.run(motorSpeed);
-  leftMotor.run(-motorSpeed);
-  delay(TURNING_TIME_MS);
-  leftMotor.run(motorSpeed);
-  delay(TURNING_TIME_MS); //need to adjust this
-  leftMotor.run(-motorSpeed);
-  delay(TURNING_TIME_MS);
-  leftMotor.stop();
-  rightMotor.stop();
+turnLeft();
+moveForward();
+delay(1500);
+turnLeft();
+StopMotor();
 }
-
 
 void doubleRightTurn() {
-  rightMotor.run(-motorSpeed);
-  leftMotor.run(motorSpeed);
-  delay(TURNING_TIME_MS);
-  rightMotor.run(motorSpeed);
-  delay(TURNING_TIME_MS); //need to adjust this
-  rightMotor.run(-motorSpeed);
-  delay(TURNING_TIME_MS);
-  leftMotor.stop();
-  rightMotor.stop();
+turnRight();
+moveForward();
+delay(1500);
+turnRight();
+StopMotor();
 }
-
 
 void nudgeLeft() {
-  // leftMotor((motorSpeed * 0.8);  // Buggy
+  leftMotor.run(left_motorSpeed * 0.7);
   delay(10);
-  // leftMotor(motorSpeed); // Buggy
+  leftMotor.run(left_motorSpeed);
 }
 
-
 void nudgeRight() {
-  // rightMotor(motorSpeed * 0.8); // Buggy
+  rightMotor.run(right_motorSpeed * 0.85);
   delay(10);
-  // rightMotor(motorSpeed);  // Buggy
+  rightMotor.run(right_motorSpeed);
+}
+
+void setup() {
+  Serial.begin(9600);
+  delay(3000);
+}
+
+void loop() {
+  moveForward();
+
+if (ultrasonic() < 4 && ultrasonic() > 1.5) {
+nudgeRight();
+}
+else if (ultrasonic() > 5 && ultrasonic() < 30) {
+  nudgeLeft();
+}
 }
