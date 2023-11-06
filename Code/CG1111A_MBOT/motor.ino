ae2fa274d426f80c4c0fc9bf0cfff9b0ee9aee8f
Ethan <familychan1@gmail.com>
2023-11-06 11:44:55 +0800

    Update motor.ino

--- Code/CG1111A_MBOT/motor.ino
+++ Code/CG1111A_MBOT/motor.ino
@@ -28,64 +28,65 @@ void wallFollower() {
   int difference = center_line - ultrasonic();
 
   if (difference > 0 && ultrasonic() > 0.1) {
-    rightMotor.run(right_motorSpeed * (1 / ((0.05 * difference) + 1)));
+    rightMotor.run(right_motorSpeed * (1 / ((0.08 * difference) + 1)));
     delay(25);
     rightMotor.run(right_motorSpeed);
 
   }
   else if (difference < 0 && ultrasonic() < 20) {
-  leftMotor.run(left_motorSpeed * (1 / ((-0.05 * difference) + 1)));
+  leftMotor.run(left_motorSpeed * (1 / ((-0.08 * difference) + 1)));
   delay(25);
   leftMotor.run(left_motorSpeed);
   } 
-  /*else if (ultrasonic() > 15 && ir_sensing_distance() == true) {
+  else if (ultrasonic() > 15 && ir_sensing_distance() == true) {
     rightMotor.run(right_motorSpeed * 0.9);
     delay(100);
     rightMotor.run(right_motorSpeed);
-  } */
+  }
 }
 
 void turnLeft() {
   rightMotor.run(right_motorSpeed);
   leftMotor.run(-left_motorSpeed);
   delay(TURNING_TIME_MS * 1.2);
-  stopMotor();
   }
 
 void turnRight() {
   leftMotor.run(left_motorSpeed);
   rightMotor.run(-right_motorSpeed);
   delay(TURNING_TIME_MS * 1.1);
-
-  stopMotor();
 }
 
 void uTurn() {
 rightMotor.run(right_motorSpeed);
   leftMotor.run(-left_motorSpeed);
   delay(TURNING_TIME_MS * 2.1);
-  stopMotor();
 }
 
 void doubleLeftTurn() {
-  turnLeft();
+ rightMotor.run(right_motorSpeed);
+  leftMotor.run(-left_motorSpeed);
+  delay(TURNING_TIME_MS * 1.2);
   wallFollower();
   delay(750);
   stopMotor();
   delay(50);
-  turnLeft();
+ rightMotor.run(right_motorSpeed);
+  leftMotor.run(-left_motorSpeed);
+  delay(TURNING_TIME_MS * 1.2);
   delay(50);
-  stopMotor();
 }
 
 void doubleRightTurn() {
-  turnRight();
+  leftMotor.run(left_motorSpeed);
+  rightMotor.run(-right_motorSpeed);
+  delay(TURNING_TIME_MS * 1.1);
   wallFollower();
   delay(750);
-  stopMotor();
-  turnRight();
+   leftMotor.run(left_motorSpeed);
+  rightMotor.run(-right_motorSpeed);
+  delay(TURNING_TIME_MS * 1.1);
   delay(50);
-  stopMotor();
 }
 
 
