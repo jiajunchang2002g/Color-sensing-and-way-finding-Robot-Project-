faa3d273628d2b3f50ff7f379289e40e80b5fd9a
Terence <zunmun@gmail.com>
2023-11-14 12:16:41 +0800

    feat: help colour not working

--- Code/CG1111A_MBOT/motor.ino
+++ Code/CG1111A_MBOT/motor.ino
@@ -2,6 +2,7 @@
 #define ULTRASONIC 12
 #define TIMEOUT 2000
 #define SPEED_OF_SOUND 330
+#define PID_SENSITIVITY 0.15
 
 int left_motorSpeed = -255;
 int right_motorSpeed = 255;
@@ -20,13 +21,13 @@ void wallFollower() {
   int difference = center_line - ultrasonic();
 
   if (difference > 0 && ultrasonic() > 0.1) {
-    rightMotor.run(right_motorSpeed * (1 / ((0.15 * difference) + 1)));
+    rightMotor.run(right_motorSpeed * (1 / ((PID_SENSITIVITY * difference) + 1)));
     delay(25);
     rightMotor.run(right_motorSpeed);
 
   }
   else if (difference < 0 && ultrasonic() < 20) {
-    leftMotor.run(left_motorSpeed * (1 / ((-0.15 * difference) + 1)));
+    leftMotor.run(left_motorSpeed * (1 / ((-PID_SENSITIVITY * difference) + 1)));
     delay(25);
     leftMotor.run(left_motorSpeed);
   } 
@@ -48,7 +49,7 @@ void turnLeft() {
 void turnRight() {
   leftMotor.run(left_motorSpeed);
   rightMotor.run(-right_motorSpeed);
-  delay(TURNING_TIME_MS * 1.1);
+  delay(TURNING_TIME_MS * 1.0);
 }
 
 void uTurn() {
@@ -67,9 +68,9 @@ if (ultrasonic() > 6) {
 void doubleLeftTurn() {
  rightMotor.run(right_motorSpeed);
   leftMotor.run(-left_motorSpeed);
-  delay(TURNING_TIME_MS * 1.1);
+  delay(TURNING_TIME_MS * 1.0); // Was 1.1
   wallFollower();
-  delay(800); 
+  delay(750); 
   stopMotor();
   delay(50);
  rightMotor.run(right_motorSpeed);
@@ -81,7 +82,7 @@ void doubleLeftTurn() {
 void doubleRightTurn() {
   leftMotor.run(left_motorSpeed);
   rightMotor.run(-right_motorSpeed);
-  delay(TURNING_TIME_MS * 1.1);
+  delay(TURNING_TIME_MS * 1.0); // Was 1.1
   wallFollower();
   delay(675); //was 750, just tweaking a bit
    leftMotor.run(left_motorSpeed);
