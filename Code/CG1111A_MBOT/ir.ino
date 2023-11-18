be6bc46c259749de2fda1cdfbda7669bf9fa03bd
jiajunchang2002g <135327869+jiajunchang2002g@users.noreply.github.com>
2023-11-18 23:50:24 +0800

    Update ir.ino

--- Code/CG1111A_MBOT/ir.ino
+++ Code/CG1111A_MBOT/ir.ino
@@ -1,28 +1,28 @@
-// new function
 bool ir_sensing_distance() {
-  shineRed();//turn off IR Emitter
-  long baseline = analogRead(IR);
-  shineIR();//turn on IR Emitter
-  long emitted_reading = analogRead(IR);
-  long diff = baseline - emitted_reading;
-  return false;
-  if (baseline >= 380 && baseline <= 480) {
+  shineRed(); //turn off IR Emitter
+  long baseVal = analogRead(IR);
+  shineIR(); 
+  long val = analogRead(IR);
+  long diff = baseVal - val;
+  
+  if (baseVal >= 380 && baseVal <= 480) {
     if (diff >= 141 && diff <= 210) {
       return true;
     }
   }
 
-  if (baseline >= 510 && baseline <= 740) {
+  if ( baseVal >= 510 && baseVal <= 740) {
     if((diff >= 210 && diff <= 350)) {
       return true;
     }
   }
   
-  if (baseline >= 870 && baseline <= 905) {
+  if (baseVal >= 870 && baseVal <= 905) {
     if (diff >= 610 && diff <= 670) {
       return true;
     }
   }
-
-  return false; //distance > 4cm
+  
+  // distance > 4cm ! 
+  return false; 
 }
