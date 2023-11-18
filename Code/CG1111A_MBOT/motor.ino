a022d84a12b366bea738109831422918e2d8a860
Terence <zunmun@gmail.com>
2023-11-19 02:41:34 +0800

    chore: standarise most constants to MACRO_CASE

--- Code/CG1111A_MBOT/motor.ino
+++ Code/CG1111A_MBOT/motor.ino
@@ -3,8 +3,8 @@
 #define TIMEOUT 2000
 #define SPEED_OF_SOUND 330
 #define PID_SENSITIVITY 0.1
-#define LEFT_MOTORSPEED -255
-#define RIGHT_MOTORSPEED 255
+#define LEFT_MOTOR_SPEED -255
+#define RIGHT_MOTOR_SPEED 255
 
 void stopMotor() {
   leftMotor.stop();
@@ -12,82 +12,82 @@ void stopMotor() {
 }
 
 void wallFollower() {
-  leftMotor.run(LEFT_MOTORSPEED);
-  rightMotor.run(RIGHT_MOTORSPEED);
+  leftMotor.run(LEFT_MOTOR_SPEED);
+  rightMotor.run(RIGHT_MOTOR_SPEED);
   double centerLine = 8.8;
   double difference = centerLine - ultrasonic();
 
   if (difference > 0 && ultrasonic() > 0.1) {
-    rightMotor.run(RIGHT_MOTORSPEED * (1 / ((PID_SENSITIVITY * difference) + 1)));
+    rightMotor.run(RIGHT_MOTOR_SPEED * (1 / ((PID_SENSITIVITY * difference) + 1)));
     delay(25);
-    rightMotor.run(RIGHT_MOTORSPEED);
+    rightMotor.run(RIGHT_MOTOR_SPEED);
 
   }
   else if (difference < 0 && ultrasonic() < 20) {
-    leftMotor.run(LEFT_MOTORSPEED * (1 / ((-PID_SENSITIVITY * difference) + 1)));
+    leftMotor.run(LEFT_MOTOR_SPEED * (1 / ((-PID_SENSITIVITY * difference) + 1)));
     delay(25);
-    leftMotor.run(LEFT_MOTORSPEED);
+    leftMotor.run(LEFT_MOTOR_SPEED);
   } 
   else if (ultrasonic() > 15 && isNearIR() == true) {
     led.setColor(255, 192, 203); 
     led.show();
-    rightMotor.run(RIGHT_MOTORSPEED * 0.9);
+    rightMotor.run(RIGHT_MOTOR_SPEED * 0.9);
     delay(100);
-    rightMotor.run(RIGHT_MOTORSPEED);
+    rightMotor.run(RIGHT_MOTOR_SPEED);
   }
 }
 
 void turnLeft() {
-  rightMotor.run(RIGHT_MOTORSPEED);
-  leftMotor.run(-LEFT_MOTORSPEED);
+  rightMotor.run(RIGHT_MOTOR_SPEED);
+  leftMotor.run(-LEFT_MOTOR_SPEED);
   delay(TURNING_TIME_MS * 1.0);
 }
 
 void turnRight() {
-  leftMotor.run(LEFT_MOTORSPEED);
-  rightMotor.run(-RIGHT_MOTORSPEED);
+  leftMotor.run(LEFT_MOTOR_SPEED);
+  rightMotor.run(-RIGHT_MOTOR_SPEED);
   delay(TURNING_TIME_MS * 0.95);
 }
 
 void uTurn() {
   if (ultrasonic() > 6) {
-    rightMotor.run(RIGHT_MOTORSPEED);
-    leftMotor.run(-LEFT_MOTORSPEED);
+    rightMotor.run(RIGHT_MOTOR_SPEED);
+    leftMotor.run(-LEFT_MOTOR_SPEED);
     delay(TURNING_TIME_MS * 1.85); 
   }
   else {
-    rightMotor.run(-RIGHT_MOTORSPEED);
-    leftMotor.run(LEFT_MOTORSPEED);
+    rightMotor.run(-RIGHT_MOTOR_SPEED);
+    leftMotor.run(LEFT_MOTOR_SPEED);
     delay(TURNING_TIME_MS * 1.85);
   }
 }
 
 void doubleLeftTurn() {
-  rightMotor.run(RIGHT_MOTORSPEED);
-  leftMotor.run(-LEFT_MOTORSPEED);
+  rightMotor.run(RIGHT_MOTOR_SPEED);
+  leftMotor.run(-LEFT_MOTOR_SPEED);
   delay(TURNING_TIME_MS * 1.1); 
-  rightMotor.run(RIGHT_MOTORSPEED);
-  leftMotor.run(0.93 * LEFT_MOTORSPEED);
+  rightMotor.run(RIGHT_MOTOR_SPEED);
+  leftMotor.run(0.93 * LEFT_MOTOR_SPEED);
   delay(900); 
   stopMotor();
   delay(50);
-  rightMotor.run(RIGHT_MOTORSPEED);
-  leftMotor.run(-LEFT_MOTORSPEED);
+  rightMotor.run(RIGHT_MOTOR_SPEED);
+  leftMotor.run(-LEFT_MOTOR_SPEED);
   delay(TURNING_TIME_MS * 1.0);
   delay(50);
-  rightMotor.run(RIGHT_MOTORSPEED);
-  leftMotor.run(LEFT_MOTORSPEED);
+  rightMotor.run(RIGHT_MOTOR_SPEED);
+  leftMotor.run(LEFT_MOTOR_SPEED);
   delay(300);
 }
 
 void doubleRightTurn() {
-  leftMotor.run(LEFT_MOTORSPEED);
-  rightMotor.run(-RIGHT_MOTORSPEED);
+  leftMotor.run(LEFT_MOTOR_SPEED);
+  rightMotor.run(-RIGHT_MOTOR_SPEED);
   delay(TURNING_TIME_MS * 0.9); 
   wallFollower();
   delay(700);
-  leftMotor.run(LEFT_MOTORSPEED);
-  rightMotor.run(-RIGHT_MOTORSPEED);
+  leftMotor.run(LEFT_MOTOR_SPEED);
+  rightMotor.run(-RIGHT_MOTOR_SPEED);
   delay(TURNING_TIME_MS * 0.9);
   delay(50);
 }
