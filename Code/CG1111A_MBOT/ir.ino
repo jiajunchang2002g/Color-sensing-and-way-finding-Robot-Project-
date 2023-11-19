78a79937da215591ab95b1b7b1e7bd917af87657
Wen Yee <wenyeechan2003@gmail.com>
2023-11-19 10:42:11 +0800

    Added a new comment for lookup table

--- Code/CG1111A_MBOT/ir.ino
+++ Code/CG1111A_MBOT/ir.ino
@@ -1,30 +1,29 @@
-// new function
-bool ir_sensing_distance() {
-  shineRed();//turn off IR Emitter
-  long baseline = analogRead(IR);
-  shineIR();//turn on IR Emitter
-  long emitted_reading = analogRead(IR);
-  long diff = baseline - emitted_reading;
-
-  if (baseline >= 380 && baseline <= 480) {
-    //if ((diff >= 190 && diff <= 210) || (diff >= 141 && diff <= 160)) {
+bool isNearIR() {
+  shineRed(); // turn off IR Emitter
+  long baseVal = analogRead(IR);
+  shineIR(); 
+  long val = analogRead(IR);
+  long diff = baseVal - val;
+  
+  // Check if the baseline ambient IR and its voltage drop fall within the thresholds which is established based on the lookup table for a distance less than 4 cm 
+  if (baseVal >= 380 && baseVal <= 480) {
     if (diff >= 141 && diff <= 210) {
       return true;
     }
   }
 
-  if (baseline >= 510 && baseline <= 740) {
-    //if((diff >= 210 && diff <= 270) && (diff >= 301 && diff <= 310) || (diff >= 330 && diff <= 350)) {
-    if((diff >= 210 && && diff <= 350)) {
+  if ( baseVal >= 510 && baseVal <= 740) {
+    if (diff >= 210 && diff <= 350) {
       return true;
     }
   }
   
-  if (baseline >= 870 && baseline <= 905) {
+  if (baseVal >= 870 && baseVal <= 905) {
     if (diff >= 610 && diff <= 670) {
       return true;
     }
   }
-
-  return false; //distance > 4cm
+  
+  // If none of the conditions are met, distance > 4cm ! 
+  return false; 
 }
